9d1ffa760dd01e27b5290a588945ac3fe06daefd
doan.kieu <doan.kieu@manifera.com>
2022-08-26 15:55:51 +0700

    update version

--- test-component/dxf_viewer_module/assets/js/index.js
+++ test-component/dxf_viewer_module/assets/js/index.js
@@ -3,4 +3,3 @@ import vueCustomElement from "vue-custom-element";
 
 Vue.use(vueCustomElement);
 Vue.customElement("summa-floorplan", require("./root.vue").default);
-// Vue.customElement("test-component", require("./test.vue").default)
